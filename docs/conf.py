6e38ee2618efd5230c1dcb80d655535ec7ccf665
Tomoaki-Ishiyama <ishiyama@chiba-u.jp>
2026-03-13 18:07:47 +0900

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'MPI-Rockstar'
-copyright = '2025, Tomoyuki Tokuue, Tomoaki Ishiyama, Ken Osato, Satoshi Tanaka, and Peter Behroozi'
+copyright = '2024, Tomoyuki Tokuue, Tomoaki Ishiyama, Ken Osato, Satoshi Tanaka, and Peter Behroozi'
 author = 'Tomoaki Ishiyama'
 release = '1.0'
 
@@ -24,42 +24,6 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
-#html_theme = 'furo'
-html_static_path = ['_static']
-## Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'MPI-Rockstar'
-copyright = '2025, Tomoyuki Tokuue, Tomoaki Ishiyama, Ken Osato, Satoshi Tanaka, and Peter Behroozi'
-author = 'Tomoaki Ishiyama'
-release = '1.0'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = [
-    'myst_parser',
-    'sphinxcontrib.mermaid',
-#    'sphinx_markdown_tables',
-    'sphinx_copybutton',
-    'sphinx_diagrams',
-]
-
-templates_path = ['_templates']
-exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
